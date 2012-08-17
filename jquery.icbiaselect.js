300216de5d214ad9b7f5db4457a163ed81ad3f75
Matthew Tretter <m@tthewwithanm.com>
2012-08-17 10:56:47 -0400

    Clean up method declarations

--- jquery.icbiaselect.js
+++ jquery.icbiaselect.js
@@ -9,95 +9,96 @@
 
 	var DATA_ATTR_PREFIX = 'data-icbiaselect-';
 
-	IcbiaSelect.prototype.initialize = function () {
-		this.$el
-			.change($.proxy(this.selectHandler, this))
-			.focus($.proxy(this.focusHandler, this))
-			.blur($.proxy(this.blurHandler, this));
-		this.wrap();
-		this.updateHitArea();
-		this.updateDisplayValue();
-	};
-
-	IcbiaSelect.prototype.blurHandler = function (event) {
-		this.wrapper.removeClass('focus');
-	};
-
-	IcbiaSelect.prototype.focusHandler = function (event) {
-		this.wrapper.addClass('focus');
-	};
-
-	IcbiaSelect.prototype.selectHandler = function (event) {
-		this.updateDisplayValue();
-	};
-
-	IcbiaSelect.prototype.updateDisplayValue = function () {
-		var label = this.$el.find('option:selected').html();
-		this.widget.find('.icbiaselect-display').html(label || '&nbsp;');
-		this.updateHitArea();
-	};
-
-	IcbiaSelect.prototype.updateHitArea = function () {
-		this.$el
-			.css({
-				width: this.widget.width(),
-				height: this.widget.height()
-			});
-	};
-
-
-	IcbiaSelect.prototype.getHtmlOptions = function () {
-		var opts = {};
-		$.each(this.$el[0].attributes, function (i, attr) {
-			if (attr.name.indexOf(DATA_ATTR_PREFIX) !== -1) {
-				opts[attr.name.substr(DATA_ATTR_PREFIX.length)] = attr.value;
-			}
-		});
-		return opts;
-	};
-
-	IcbiaSelect.prototype.defaultOptions = {
-		widgetTemplate: function () {
-			return $(' ' +
-					'<span>                                                            ' +
-					'    <span class="icbiaselect-display-wrapper">                    ' +
-					'        <span class="icbiaselect-display"></span>                 ' +
-					'    </span>                                                       ' +
-					'    <span class="icbiaselect-arrow"><i></i></span>                ' +
-					'</span>                                                           '
-				);
+	IcbiaSelect.prototype = {
+		initialize: function () {
+			this.$el
+				.change($.proxy(this.selectHandler, this))
+				.focus($.proxy(this.focusHandler, this))
+				.blur($.proxy(this.blurHandler, this));
+			this.wrap();
+			this.updateHitArea();
+			this.updateDisplayValue();
 		},
-		a: 3
-	};
 
-	IcbiaSelect.prototype.createWidget = function () {
-		var wt = this.options.widgetTemplate,
-		    widget = typeof wt === 'function' ? wt() : $($(wt).html());
+		blurHandler: function (event) {
+			this.wrapper.removeClass('focus');
+		},
 
-		return widget
-			.addClass('icbiaselect-widget');
-	};
+		focusHandler: function (event) {
+			this.wrapper.addClass('focus');
+		},
 
-	IcbiaSelect.prototype.wrap = function () {
-		if (!this.wrapper) {
-			var parent = this.$el.parent();
-			this.widget = this.createWidget();
-			this.wrapper = $('<span class="icbiaselect"></span>')
+		selectHandler: function (event) {
+			this.updateDisplayValue();
+		},
+
+		updateDisplayValue: function () {
+			var label = this.$el.find('option:selected').html();
+			this.widget.find('.icbiaselect-display').html(label || '&nbsp;');
+			this.updateHitArea();
+		},
+
+		updateHitArea: function () {
+			this.$el
 				.css({
-					position: 'relative',
-					display: 'inline-block'
-				})
-				.append(this.widget)
-				.append(
-					this.$el.css({
-						position: 'absolute',
-						border: '1px solid #fff',  // To force the dimensions to take effect.
-						top: 0,
-						left: 0,
-						opacity: 0
+					width: this.widget.width(),
+					height: this.widget.height()
+				});
+		},
+
+		getHtmlOptions: function () {
+			var opts = {};
+			$.each(this.$el[0].attributes, function (i, attr) {
+				if (attr.name.indexOf(DATA_ATTR_PREFIX) !== -1) {
+					opts[attr.name.substr(DATA_ATTR_PREFIX.length)] = attr.value;
+				}
+			});
+			return opts;
+		},
+
+		defaultOptions: {
+			widgetTemplate: function () {
+				return $(' ' +
+						'<span>                                                            ' +
+						'    <span class="icbiaselect-display-wrapper">                    ' +
+						'        <span class="icbiaselect-display"></span>                 ' +
+						'    </span>                                                       ' +
+						'    <span class="icbiaselect-arrow"><i></i></span>                ' +
+						'</span>                                                           '
+					);
+			},
+			a: 3
+		},
+
+		createWidget: function () {
+			var wt = this.options.widgetTemplate,
+			    widget = typeof wt === 'function' ? wt() : $($(wt).html());
+
+			return widget
+				.addClass('icbiaselect-widget');
+		},
+
+		wrap: function () {
+			if (!this.wrapper) {
+				var parent = this.$el.parent();
+				this.widget = this.createWidget();
+				this.wrapper = $('<span class="icbiaselect"></span>')
+					.css({
+						position: 'relative',
+						display: 'inline-block'
 					})
-				)
-				.appendTo(parent);
+					.append(this.widget)
+					.append(
+						this.$el.css({
+							position: 'absolute',
+							border: '1px solid #fff',  // To force the dimensions to take effect.
+							top: 0,
+							left: 0,
+							opacity: 0
+						})
+					)
+					.appendTo(parent);
+			}
 		}
 	};
 
